6fa83329a13d44d688792444667afe886ea46830
Ankit Navral <navralankit@gmail.com>
2024-04-17 07:06:13 +0000

    add materialized view feature

---
+++ models/dishank_test/dishank_materialized_view_test.sql
@@ -0,0 +1,16 @@
+{{ config(
+    materialized="dynamic_table",
+    on_configuration_change="apply",
+    target_lag="10 seconds",
+    snowflake_warehouse="COMPUTE_WH",
+) }}
+
+with source_store_sales as (
+    select * from {{source('DISHANK_SOURCE','DISHANK_SOURCE')}}
+),
+
+final as (
+    select TOP 6 * from source_store_sales
+)
+
+select * from final
